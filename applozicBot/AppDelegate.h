bc0d7932468e0e776b87517b542479096bdf6758
Divjyot <divjyot_sngh@hotmail.com>
2016-07-30 13:52:42 +0530

    initial commit

---
+++ applozicBot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  applozicBot
+//
+//  Created by Divjyot Singh on 30/07/16.
+//  Copyright © 2016 Divjyot Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ applozicBot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  applozicBot
+//
+//  Created by Divjyot Singh on 30/07/16.
+//  Copyright © 2016 Divjyot Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
